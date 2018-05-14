6b7b66405c612bed25153944b77fb18418d8d359
it-coral <coral1212@yahoo.com>
2018-05-14 18:30:38 +0300

    sendgrid setting

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,2 +1,14 @@
+Rails.application.config.action_mailer.smtp_settings = {enable_starttls_auto: false}
 Rails.application.config.action_mailer.default_url_options = { host: APP_CONFIG['host'] }
-Rails.application.config.action_mailer.smtp_settings = { enable_starttls_auto: false }
+unless Rails.env.test?
+  Rails.application.config.action_mailer.delivery_method = :smtp
+  Rails.application.config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USER'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: APP_CONFIG['host'],
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+end
